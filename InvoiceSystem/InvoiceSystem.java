af84f2fb0a76b9ab3235a9bdb4cdaae6b7321ec0
cmssanthosh26 <77882030+cmssanthosh26@users.noreply.github.com>
2021-02-10 10:58:49 +0530

    Update InvoiceSystem.java

--- InvoiceSystem/InvoiceSystem.java
+++ InvoiceSystem/InvoiceSystem.java
@@ -4,16 +4,10 @@ public class InvoiceSystem
 {
     public static void main(String[] args) 
     {
-        /*String[] name={"Santhosh","Soundar","Mohan"};
-        String[] mno={"8072023642","9159563724","9876543210"};*/
-        
         String[] item={"Dosa","Poori","Rice","Briyani"};
         float[] itemRate={10,20,40,70};
         InvoiceControl ic=new InvoiceControl();
         ic.initializeItem(item,itemRate);
-        //ic.initializeCustomer(name, mno);
         ic.startExecution();
     }    
 }
-
-
